d1fa233feae01316a0817fbe8d172d830c144bdf
psyGamer <psygamer21@gmail.com>
2023-10-22 12:14:53 +0200

    Removed old spreadsheets

--- spreadsheet/main.py
+++ spreadsheet/main.py
@@ -4,10 +4,8 @@ import gspread
 from util import ChapterTime, format_frames
 
 sheet_name = "Ultra Jump TAS - Statistics"
-anypercent_name = "any%"
-trueending_name = "All A-Sides / True Ending"
-il_worksheet = "testing 1"
-fullgame_worksheet="testing 2"
+il_worksheet = "Individuel Levels"
+fullgame_worksheet = "Fullgame"
 
 gc: gspread.client.Client = None
 if "GITHUB_WORKFLOW" in os.environ:
@@ -67,9 +65,9 @@ def fill_out_diff(updates, formats, times_a, times_b, column, row, files_a, file
                 text = f"{prefix}{str(time)}"
 
             if time.negative:
-                color = color_green
+                color = green
             else:
-                color = color_red
+                color = red
 
         cell = f"{column}{row + i}"
         updates.append(gspread.cell.Cell.from_address(cell, text))
@@ -110,11 +108,12 @@ def update_fullgame_times():
     updates = []
     formats = []
 
+    green = { "red": 0.145, "green": 0.485, "blue": 0.0 }
     files = [ "0 - Any%.tas", "0 - Bny%.tas", "0 - 100%.tas", "0 - True Ending.tas", "0 - All A Sides.tas", "0 - All B Sides.tas", "0 - All C Sides.tas", "0 - All Chapters.tas", "0 - All Red Berries.tas", "0 - All Hearts.tas", "0 - All Cassettes.tas"]
     
     fill_out_time(updates, maingame_times, "J", 6, files)
     fill_out_time(updates, all_times, "K", 6, files)
-    fill_out_diff(updates, formats, all_times, maingame_times, "L", 6, files, files, only_frames=False, green={ "red": 0.145, "green": 0.485, "blue": 0.0 })
+    fill_out_diff(updates, formats, all_times, maingame_times, "L", 6, files, files, only_frames=False, green=green)
 
     sheet.update_cells(updates)
     sheet.batch_format(formats)
@@ -155,98 +154,6 @@ def update_fullgame_routes():
     fill_out_time(updates, all_times, "D", 44, ac_b_files)
     fill_out_diff(updates, formats, all_times, all_times, "E", 44, ac_b_files, a_files, green=green)
     
-    sheet.update_cells(updates)
-    sheet.batch_format(formats)
-
-def update_anypercent():
-    chapters = ["1", "2", "3", "4", "5", "6", "7"]
-    times = copy.deepcopy(all_times)
-
-    # Merge B into AC
-    for c in chapters:
-        b = f"{c}B.tas"
-        ac = f"{c}AC.tas"
-        if not b in times or not ac in times:
-            continue
-        times[ac] = ChapterTime.from_frames(times[ac].frames + times[b].frames)
-
-    # Update spreadsheet
-    sheet = sh.worksheet(anypercent_name)
-    updates = []
-    formats = []
-
-    a_files = ["1A.tas", "2A.tas", "3A.tas", "4A.tas", "5A.tas", "6A.tas", "7A.tas" ]
-    ac_files = ["1AC.tas", "2AC.tas", "3AC.tas", "4AC.tas", "5AC.tas", "6AC.tas", "7AC.tas" ]
-    b_files = ["1B.tas", "2B.tas", "3B.tas", "4B.tas", "5B.tas", "6B.tas", "7B.tas" ]
-    fill_out_time(updates, formats, files, "B", a_files)
-    fill_out_time(updates, formats, files, "C", ac_files)
-    fill_out_time(updates, formats, files, "E", b_files)
-    fill_out_diff(updates, formats, files, "D", a_files, ac_files)
-
-    # sheet.update_cells(updates)
-    # sheet.batch_format(formats)
-
-def update_trueending():
-    chapters = ["1", "2", "3", "4", "5", "6", "7"]
-    files = copy.deepcopy(all_times)
-
-
-    # Merge B-Side into AC/HC
-    for f in all_times:
-        if not f.endswith("B.tas"):
-            continue
-        ac = f"{f[0]}AC.tas"
-        hc = f"{f[0]}HC.tas"
-        if not f in files:
-            continue
-        if ac in files:
-          files[ac]["time"] = ChapterTime.from_frames(files[ac]["time"].frames + files[f]["time"].frames)
-        if hc in files:
-          files[hc]["time"] = ChapterTime.from_frames(files[hc]["time"].frames + files[f]["time"].frames)
-
-    # # Select faster version by color
-    # for c in chapters:
-    #     a = f"{c}A.tas"
-    #     ac = f"{c}AC.tas"
-    #     b = f"{c}B.tas"
-    #     if not a in files and not ac in files:
-    #         continue # Nothing drafted
-    #     elif a in files and not ac in files:
-    #         files[a]["color"] = color_green # Only A-Side Drafted
-    #     elif not a in files and ac in files:
-    #         files[ac]["color"] = color_green # Only B-Side Drafted    
-    #     elif a in files and ac in files:
-    #         a_frames = files[a]["time"].frames
-    #         ac_frames = files[ac]["time"].frames
-    #         if a_frames < ac_frames:
-    #             files[a]["color"] = color_green
-    #         elif ac_frames < a_frames:
-    #             files[ac]["color"] = color_green
-    #         else:
-    #             files[a]["color"] = color_green
-    #             files[ac]["color"] = color_green
-    #     # Mark B-Side red if slower than A-Side alone
-    #     if not a in files or not b in files:
-    #         continue
-    #     a_frames = files[a]["time"].frames
-    #     b_frames = files[b]["time"].frames
-    #     if b_frames > a_frames:
-    #         files[b]["color"] = color_red
-
-    # Update spreadsheet
-    sheet = sh.worksheet(trueending_name)
-
-    updates = []
-    formats = []
-    fill_out_time(updates, formats, files, "B", a_files)
-    fill_out_time(updates, formats, files, "C", h_files)
-    fill_out_time(updates, formats, files, "E", ac_files)
-    fill_out_time(updates, formats, files, "G", hc_files)
-
-    fill_out_diff(updates, formats, files, "D", h_files, a_files)
-    fill_out_diff(updates, formats, files, "F", ac_files, a_files)
-    fill_out_diff(updates, formats, files, "H", hc_files, a_files)
-
     sheet.update_cells(updates)
     sheet.batch_format(formats)
 
@@ -301,8 +208,6 @@ if __name__ == "__main__":
         if b in all_times and ac in all_times:
             all_times[ac_b] = ChapterTime.from_frames(all_times[ac].frames + all_times[b].frames)
 
-    # update_anypercent()
-    # update_trueending()
-    # update_il()
-    # update_fullgame_times()
+    update_il()
+    update_fullgame_times()
     update_fullgame_routes()
